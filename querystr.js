4a76826516dda30eee5f96f5057afaba6baa7e41
斟酌 鵬兄 <tgckpg@gmail.com>
2016-06-16 15:30:40 +0800

    using the native api

--- querystr.js
+++ querystr.js
@@ -1,19 +1,11 @@
 var cl = global.botanLoader;
 var CondStream = cl.load( "botanss.utils.CondStream" );
+var querystr = require( "querystring" );
 
 module.exports = {
 	queryStr: function( qstr )
 	{
-		var qObj = {};
-
 		if( qstr instanceof CondStream ) qstr = qstr.toString();
-
-		qstr.split( "&" ).forEach( function( val )
-		{
-			val = val.split( "=" );
-			qObj[ val[0] ] = val[1] ? decodeURIComponent( val[1].replace( /\+/g, " " ) ) : "";
-		} );
-
-		return qObj;
+		return querystr.parse( qstr );
 	}
 };
